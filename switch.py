36bb55d8128867aa286b033d736dc8867ff75cf1
Stalyn <estalinxk3gmail.com>
2022-04-26 08:35:32 -0500

    comandos py borar espcaios, mostrar partes específicas de un string y alrevez letras

--- switch.py
+++ switch.py
@@ -21,3 +21,31 @@ elif opcion == 3:
 else:
     print('ingresá nu´meros del 1 al tres nmms')
 
+>>> nomre = input ("¿cuál es tu nombre ?")
+¿cuál es tu nombre ?facundo
+>>> nombre.capitaliza()
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'nombre' is not defined. Did you mean: 'nomre'?
+>>> nomre.capitalize() 
+'Facundo'
+>>> nombre = "francisco "
+>>> nombre 
+'francisco '
+>>> nombre[0]
+'f'
+>>> nombre[0:3] 
+'fra'
+>>> nombre[3:5]
+'nc'
+>>> nombre[3:5:1]
+'nc'
+>>> nombre[2:5]
+'anc'
+>>> nombre[::-1] 
+' ocsicnarf'
+>>> nombre = nombre.strip()
+>>> nombre
+'francisco'
+>>> nombre[::-1]
+'ocsicnarf'
