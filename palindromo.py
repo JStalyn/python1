bf7d7a482a759d3badffda25e7c40a0d35660e6c
Stalyn <estalinxk3gmail.com>
2022-04-26 11:11:17 -0500

    programa palindromo

---
+++ palindromo.py
@@ -0,0 +1,24 @@
+from ast import If
+
+
+def palindromo(palabra):
+    palabra = palabra.replace(' ','')
+    palabra = palabra.lower()
+    palabra_invertida = palabra[::-1]
+    if palabra == palabra_invertida:
+        return True
+    else:
+        return False
+
+
+def run():
+    palabra =input("escribe una palabra: ")
+    es_palindromo = palindromo(palabra)
+    if es_palindromo== True:
+        print('es palindromo')
+    else:
+        print('No es palíndromo')
+
+        
+if __name__== '__main__':
+    run()
