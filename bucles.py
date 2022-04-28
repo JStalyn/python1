883721cd6df83b4c09fe8b6a4f0eaa845f4164e7
Stalyn <estalinxk3gmail.com>
2022-04-28 08:10:09 -0500

    bucles, contadore del 2 elevado hasta 500

---
+++ bucles.py
@@ -0,0 +1,35 @@
+# contador = 0
+# print('2 elevado a la ' +str(contador) + ' = '+ str(2**contador))
+
+
+# def run():
+#    LIMITE = 1000000
+#    contador = 0
+#    potencia_2 = 2**contador
+
+#    while potencia_2 < LIMITE:
+#         print('2 elevado a la ' +str(contador) + ' = '+ str(potencia_2))
+#         contador = contador +1
+#         potencia_2 = 2**contador
+
+
+# if __name__ == '__main__':
+#     run()
+
+
+
+def run():
+    numero = 0
+    LIMITE = 1000
+    pontencia = 2**numero
+    print('2')
+    
+    while pontencia < LIMITE:
+     
+        print('2 elevado a '+ str(numero)+' = '+str(pontencia))
+        numero =numero+1
+        pontencia = 2**numero
+
+
+if __name__ == '__main__':
+    run()
